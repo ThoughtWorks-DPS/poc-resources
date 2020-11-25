c43632f06e888a689b1ff0e31b4bbfb2b158c396
Punit Lad <punitlad@gmail.com>
2020-11-25 15:25:40 -0500

    [Punit] scan for secrets in specified repo

--- scan_repos_for_secrets.sh
+++ scan_repos_for_secrets.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
-function scanRepository() {
-  echo scanning repository
+installGitSecrets() {
+  curl -sL https://raw.githubusercontent.com/awslabs/git-secrets/master/git-secrets >> git-secrets
+  chmod +x git-secrets
+  git config --global --unset secrets.patterns
 }
 
-scanRepository
+scanRepository() {
+  cd $1
+  ./../git-secrets --add-provider -- cat ../git-secrets-pattern.txt > /dev/null
+  ./../git-secrets --scan --recursive
+}
+
+fetchRepository() {
+  git clone git@github.com:ThoughtWorks-DPS/$1.git $1
+}
+
+installGitSecrets
+echo "Scanning secrets for repository: $1"
+echo "------------------------"
+fetchRepository $1
+scanRepository $1
+echo "Exit Code: $?"
