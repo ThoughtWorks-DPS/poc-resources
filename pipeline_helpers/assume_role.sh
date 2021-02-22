d6c674b2828f82064a494720be9a97b439596dde
Seun Oluwaloju <seunoluwaloju@Seuns-MacBook-Pro.local>
2021-02-22 16:35:18 -0600

    [Seun | Punit | Nelson] - refactor script to include arn and account id variable

--- pipeline_helpers/assume_role.sh
+++ pipeline_helpers/assume_role.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 ROLE=$1
 SESSION_NAME=$2
 
-aws sts assume-role --output json --role-arn $ROLE --role-session-name $SESSION_NAME --duration-seconds 900 > credentials
+aws sts assume-role --output json --role-arn arn:aws:iam::$AWS_ACCOUNT_ID:role/$ROLE --role-session-name $SESSION_NAME --duration-seconds 900 > credentials
 
 export AWS_ACCESS_KEY_ID=$(cat credentials | jq -r ".Credentials.AccessKeyId")
 export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAccessKey")
