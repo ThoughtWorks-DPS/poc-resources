4b1b6f9b7a33e73efd05c1bb61d74f8525cef74d
Gary Fong <gary.fong@thoughtworks.com>
2021-04-07 15:26:16 -0700

    [GF|CN] Updated env var injection script to handle multi-line env var values.

--- pipeline_helpers/inject_env_vars_to_file.sh
+++ pipeline_helpers/inject_env_vars_to_file.sh
@@ -3,10 +3,11 @@
 runpath=$(dirname $0)
 templateFile=""
 outputFile=""
+sedDelimiter='%'
 
 function usage {
-    echo "$0 [--var|-v <key value>] [--in-file|-i <file_to_inject.json.tpl>] [--out-file|-o <output_file.json>]"
-    echo "  --var|-v      key-value pair to inject (key value)"
+    echo "$0 [--var|-v <key> <value>] [--in-file|-i <file_to_inject.json.tpl>] [--out-file|-o <output_file.json>]"
+    echo "  --var|-v      key-value pair to inject [will override keys from environment] (key value)"
     echo "  --in-file|-i  file to inject into"
     echo "  --out-file|-o output file"
     echo "  --help        display this help"
@@ -16,29 +17,40 @@ function usage {
 function inject_env_vars_to_file {
     local file=$1
     local sedFile=$2
-    local outputFile=${3:-${file//.tpl}}
+    local outputFile=${3:-${$(basename file)//.tpl}}
     sed -f "${sedFile}" "${file}" > "${outputFile}"
 }
 
 function create_sed_from_env {
     local file=$1
-    env | sed -e "s:\(.*\)=\(.*\):s%{{[ ]*\1[ ]*}}%\2%:" > "${file}"
+    for i in `compgen -v`
+    do
+        if [ ! "${i}" == "IFS" -a ! "${i}" == "COMP_WORDBREAKS" -a ! "${i}" == "sedDelimiter" ];
+        then
+            local key=${i}
+            local value=$(echo "${!i}" | sed -e '$ ! s/$/\\/')
+            # Remove this when we fix how we store the REVOCATION_CERT in AWS Secrets
+            #if [ "${i}" == "REVOCATION_CERT" ]; then value="${value}
+#Comment: Fix newline"; fi
+
+            echo "s${sedDelimiter}{{[ ]*${key}[ ]*}}${sedDelimiter}${value}${sedDelimiter}" >> "${file}"
+        fi
+    done
 }
 
 sedfile=$(mktemp /tmp/sedfile.XXXXXX)
-create_sed_from_env "${sedfile}"
 
 while [ $# -gt 0 ]
 do
     case $1 in
     --var|-v)
-        shift; 
+        shift;
         key=$1;
         value=$2;
         echo "s%{{[ ]*$key[ ]*}}%$value%" >> "${sedfile}"
         shift;;
     --in-file|-i)
-        shift; 
+        shift;
         templateFile=$1;;
     --out-file|-o)
         shift;
@@ -47,7 +59,9 @@ do
     *) usage; exit -1;;
     esac
     shift;
-done 
+done
+
+create_sed_from_env "${sedfile}"
 
 inject_env_vars_to_file "${templateFile}" "${sedfile}" "${outputFile}"
 
